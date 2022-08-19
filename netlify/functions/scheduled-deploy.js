ea1eba71240328c331a2773f8c1b22329ab37322
Ben Hong <ben@bencodezen.io>
2022-08-19 09:49:18 -0700

    disable schcedule

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,12 +4,13 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/62fefde419ae1d2c11e30a2d'
 
-const handler = schedule('* * * * *', async () => {
-  await fetch(BUILD_HOOK, {
-    method: 'POST'
-  }).then(response => {
-    console.log('Build hook response:', response)
-  })
+const handler = schedule('* 1 * * *', async () => {
+  console.log('This works!')
+  // await fetch(BUILD_HOOK, {
+  //   method: 'POST'
+  // }).then(response => {
+  //   console.log('Build hook response:', response)
+  // })
 
   return {
     statusCode: 200
